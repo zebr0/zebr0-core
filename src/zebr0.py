4e6280728c9e94231c3ea94d4f3fa691059950e0
mazerty <mazerty@gmail.com>
2018-09-20 10:05:10 +0200

    merged zebr0-template with zebr0-core: now the templating occurs by default in the lookup, that allows for value to reference zebr0 variables or the defined filters

--- src/zebr0.py
+++ src/zebr0.py
@@ -1,8 +1,12 @@
 import argparse
+import hashlib
+import json
 import logging
 import os.path
+import subprocess
 import sys
 
+import jinja2
 import requests
 
 
@@ -47,11 +51,29 @@ class ArgumentParser(argparse.ArgumentParser):
         return args
 
 
+def sh(command):
+    return subprocess.run(command, shell=True, stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+
+
+def hash(string, algorithm="md5"):
+    return hashlib.new(algorithm, string.encode("utf-8")).hexdigest()
+
+
 class Service:
     def __init__(self, args):
         self.url = args.url
         self.project = args.project
         self.stage = args.stage
+
+        # sets up the template environment
+        self.environment = jinja2.Environment(keep_trailing_newline=True)
+        self.environment.globals["url"] = args.url
+        self.environment.globals["project"] = args.project
+        self.environment.globals["stage"] = args.stage
+        self.environment.filters["json"] = json.loads
+        self.environment.filters["lookup"] = self.lookup
+        self.environment.filters["sh"] = sh
+        self.environment.filters["hash"] = hash
 
         # sets up a basic value cache, to avoid downloading the same value twice
         self._cache = {}
@@ -59,23 +81,25 @@ class Service:
         # initializes the logger
         self._logger = logging.getLogger(__name__ + "." + __class__.__name__)
 
-    def lookup(self, key, strip=True):
+    def lookup(self, key, default=None, render=True, strip=True):
         """
         Looks for the value of the given key in the remote repository.
         The value is then stored in a local cache to speed up subsequent calls.
 
         :param key: key to look for
+        :param default: if specified, returns this value instead of raising an error if the key isn't found
+        :param render: whether to render through jinja2 the content of the value or not
         :param strip: whether to strip the value off leading and trailing whitespaces or not
         :return: value of the given key in the remote repository
         """
 
         if not self._cache.get(key):
             # if the key isn't cached, looks for it in the remote repository, then stores it
-            self._cache[key] = self._remote_lookup(key, strip)
+            self._cache[key] = self._remote_lookup(key, default, render, strip)
 
         return self._cache.get(key)
 
-    def _remote_lookup(self, key, strip):
+    def _remote_lookup(self, key, default, render, strip):
         self._logger.info("looking for key '%s' in remote repository", key)
 
         # first it will look for the key at the most specific level: url/project/stage
@@ -85,7 +109,11 @@ class Service:
                      [self.url, key]]:
             response = requests.get("/".join(path))
             if response.ok:
-                return response.text.strip() if strip else response.text
+                value = response.text if not render else self.environment.from_string(response.text).render()
+                return value if not strip else value.strip()
 
-        # if not, raises an error
-        raise LookupError("key '{}' not found anywhere for project '{}', stage '{}' in '{}'".format(key, self.project, self.stage, self.url))
+        # if not, returns the default value is specified, else raises an error
+        if default:
+            return default
+        else:
+            raise LookupError("key '{}' not found anywhere for project '{}', stage '{}' in '{}'".format(key, self.project, self.stage, self.url))
--- test/test.sh
+++ test/test.sh
@@ -1,26 +1,77 @@
 #!/bin/sh -ex
 
 # cleans a potentially failed previous test run
+[ -f tmp/pid ] && kill $(cat tmp/pid)
 [ -d tmp/ ] && rm -rf tmp/
 
 # creates tmp directory
 mkdir tmp
 
-# first test: creating the parameter files
-../src/zebr0-init -c tmp -u https://raw.githubusercontent.com/zebr0/zebr0-conf/master -p dummy_project -s dummy_stage
+# starts the mock server
+cd mock
+python3 -m http.server &
+echo $! > ../tmp/pid
+sleep 1
+cd ..
+
+# zebr0-init test 1: creating the parameter files
+../src/zebr0-init -c tmp -u http://localhost:8000 -p dummy_project -s dummy_stage
 diff tmp/url results/url
 diff tmp/project results/project
 diff tmp/stage results/stage
 
-# second test: idempotence when reading from the parameter files
+# zebr0-init test 2: idempotence when reading from the parameter files
 ../src/zebr0-init -c tmp
 diff tmp/url results/url
 diff tmp/project results/project
 diff tmp/stage results/stage
 
-# third test: fetching the value of a key from the remote repository
-../src/zebr0-lookup -c tmp LICENSE > tmp/LICENSE
-diff tmp/LICENSE ../LICENSE
+# lookup test 1: fetching the value of a key from the remote repository
+../src/zebr0-lookup -c tmp test-key > tmp/test-key
+diff tmp/test-key results/test-key
+
+# lookup test 2: render
+../src/zebr0-lookup -c tmp test-key --render > tmp/render
+diff tmp/render results/render
+
+# lookup test 3: strip
+../src/zebr0-lookup -c tmp test-key --strip > tmp/strip
+diff tmp/strip results/strip
+
+# lookup test 4: project key
+../src/zebr0-lookup -c tmp project-key > tmp/project-key
+diff tmp/project-key results/project-key
+
+# lookup test 5: stage key
+../src/zebr0-lookup -c tmp stage-key > tmp/stage-key
+diff tmp/stage-key results/stage-key
+
+# lookup test 6: json filter
+../src/zebr0-lookup -c tmp json --render > tmp/json
+diff tmp/json results/json
+
+# lookup test 7: lookup filter
+../src/zebr0-lookup -c tmp lookup --render > tmp/lookup
+diff tmp/lookup results/render
+
+# lookup test 8: sh filter
+../src/zebr0-lookup -c tmp sh --render > tmp/sh
+diff tmp/sh results/sh
+
+# lookup test 9: hash filter
+../src/zebr0-lookup -c tmp hash --render > tmp/hash
+diff tmp/hash results/hash
+
+# lookup test 10: default value
+../src/zebr0-lookup -c tmp unknown-key --default "default value" > tmp/default
+diff tmp/default results/default
+
+# lookup test 11: default value in lookup filter
+../src/zebr0-lookup -c tmp default --render > tmp/default2
+diff tmp/default2 results/default
+
+# stops the mock server
+kill $(cat tmp/pid) && rm tmp/pid
 
 # cleans tmp directory
 rm -rf tmp
